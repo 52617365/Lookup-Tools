57d49f6cd55103d9af3c1f4954eeaf5724c2b585
52617365 <rasengithub@protonmail.com>
2022-11-10 16:15:18 +0200

    got rid of useless files, filereader handles them all.

--- file/file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def is_valid_file(file_path):
-    return os.path.isfile(file_path)
-
-
-def get_file_without_path_or_extension(file_name):
-    return os.path.splitext(os.path.basename(file_name))[0]
