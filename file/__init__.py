9bf8bd3cce0c853c876545a3b63f95ff16ee149b
52617365 <rasengithub@protonmail.com>
2022-11-10 17:49:20 +0200

    TODO's added in __init__.py

--- file/__init__.py
+++ file/__init__.py
@@ -1,2 +1,2 @@
 # TODO: We should generate a SHA256 hash of the files that we iterate and store it in a file. This file should then be checked to avoid checking that file again.
-
+# TODO: We should maybe differentiate between valid and invalid files. Valid files are files that are in a correct format. Invalid files are files that are in an invalid format.
