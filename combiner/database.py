0aef1c2da9ae1d788e38bfa56a6aceb0860f7ac4
52617365 <rasengithub@protonmail.com>
2022-11-10 16:14:40 +0200

    database.py now uses FileReader in ctor.

--- combiner/database.py
+++ combiner/database.py
@@ -1,36 +1,36 @@
 from pandas import DataFrame
 
-from file.csv import read_csv_file
-from file.file import get_file_without_path_or_extension
+from file.filereader import FileReader
 
 
 class Database:
     def __init__(self, path_to_database, additional_database_information):
-        self.__database_name = get_file_without_path_or_extension(path_to_database)
+        self.__file = FileReader(path_to_database)
+        self.__database_name = self.__file.get_file_name()
+        self.__additional_information = additional_database_information
         try:
-            self.__additional_information = additional_database_information
-            self.__database_contents = read_csv_file(path_to_database)
+            self.__database_contents = self.__file.get_file_as_csv()
         except Exception as e:
+            # TODO: maybe add the file to some invalid/* folder for future investigation?
             raise e
 
     def combine(self) -> DataFrame:
         self.__set_additional_information_to_database()
         return self.__database_contents
 
-    def get_database_name(self) -> str:
-        return self.__database_name
+    # def get_database_name(self) -> str:
+    #     return self.__database_name
+    #
+    # def get_database_contents(self) -> DataFrame:
+    #     return self.__database_contents
 
-    def get_database_contents(self) -> DataFrame:
-        return self.__database_contents
-
-    def get_additional_information(self) -> (str, str):
+    def get_additional_information(self) -> str:
         breach_date = self.__get_breach_date_from_additional_database_information()
-        database_name = self.get_database_name()
-        return breach_date, database_name
+        return breach_date
 
     def __set_additional_information_to_database(self):
-        breach_date, database_name = self.get_additional_information()
-        self.__database_contents["database_name"] = database_name
+        breach_date = self.get_additional_information()
+        self.__database_contents["database_name"] = self.__database_name
         if breach_date is not None:
             self.__database_contents["breach_date"] = breach_date
 
@@ -44,8 +44,9 @@ class Database:
 
 if __name__ == '__main__':
     try:
-        additional_information = read_csv_file("../list_of_leaks.txt")
-        database = Database("../000webhost.com.csv", additional_information)
+
+        additional_information = FileReader("../list_of_leaks.txt")
+        database = Database("../000webhost.com.csv", additional_information.get_file_as_csv())
         combined_database = database.combine()
         print(combined_database)
     except Exception as e:
