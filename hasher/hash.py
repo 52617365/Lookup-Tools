2924cad359ddf4c508c419ba1730ea6ac22a42e0
52617365 <rasengithub@protonmail.com>
2022-11-12 09:19:31 +0200

    Added function to hash.py to write the unique id to file.

--- hasher/hash.py
+++ hasher/hash.py
@@ -11,13 +11,7 @@ class Hasher:
     def __get_sha256_hash(self) -> str:
         return hashlib.sha256(self.__file_data.encode('utf-8')).hexdigest()
 
-    def write_file_identifier_to_hashes(self):
+    def write_unique_identifier_of_file_to_logs(self):
         # This is not wise to open for each write, maybe pss this writer into the class?
-        writer = CsvWriter(self.__hash_file_path, self.__get_sha256_hash())
-        writer.write_hash_to_file()
-
-
-if __name__ == '__main__':
-    df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
-    hasher = Hasher(df.to_csv())
-    hasher.write_file_identifier_to_hashes()
+        with open(self.__hash_file_path, "a") as hashes:
+            hashes.write(self.__get_sha256_hash() + "\n")
