a01030e112d16dd606286a122c7ed65ffbdfba50
52617365 <rasengithub@protonmail.com>
2022-11-13 09:54:50 +0200

    Added important TODOs related to error handling that need to be thought out asap.

--- file/filereader.py
+++ file/filereader.py
@@ -2,6 +2,7 @@ import os
 import warnings
 
 import pandas as pd
+from pandas import DataFrame
 from pandas.errors import ParserWarning
 
 
@@ -22,13 +23,20 @@ class FileReader:
         try:
             # We specify that the engine is python because the C engine is not able to determine the dynamic delimiter.
             # We have to turn the warning into an error here because the python engine shows a warning instead of an error.
-            with warnings.catch_warnings():
-                warnings.simplefilter("error", category=ParserWarning)
-                csv_file = pd.read_csv(self.__file_path, engine='python', sep=None, skipinitialspace=True,
-                                       index_col=False)
-                return csv_file
+            csv_file: DataFrame = self.get_csv_with_custom_delimiter_turning_warnings_into_errors()
+            return self.get_csv_with_custom_delimiter_turning_warnings_into_errors()
         except Exception as invalid_format:
+            # TODO: mark the file as invalid somehow.
+            # Maybe have another hash file that contains the hashes to invalid files.
+            # We want to handle that here to avoid passing exceptions around.
             raise invalid_format
+
+    def get_csv_with_custom_delimiter_turning_warnings_into_errors(self) -> DataFrame:
+        with warnings.catch_warnings():
+            warnings.simplefilter("error", category=ParserWarning)
+            csv_file = pd.read_csv(self.__file_path, engine='python', sep=None, skipinitialspace=True,
+                                   index_col=False)
+            return csv_file
 
     def get_file_as_txt(self) -> str:
         with open(self.__file_path, 'r') as file:
