835fe330d3c1d9d4ee66c1c451962c073503696e
52617365 <rasengithub@protonmail.com>
2022-11-17 14:45:30 +0200

    Renamed user_arguments.py to UserArguments.py.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- main/UserArguments.py
+++ main/UserArguments.py
@@ -7,9 +7,11 @@ class CommandLineArguments:
         self.parser = argparse.ArgumentParser(description='Parse delimited files into JSON strings')
         self.parser.add_argument('-i', '--input', type=str, help='The delimited file that should be parsed to JSON',
                                  required=True)
+        self.parser.add_argument('-a', '--additional', type=str,
+                                 help='The path to the additional database information', required=True)
         self.parser.add_argument('-g', '--glob', default=False, type=bool,
                                  help='A boolean value to determine if the input is a glob', required=False)
         self.args = self.parser.parse_args()
 
-    def get_arguments(self):
+    def get(self):
         return self.args
