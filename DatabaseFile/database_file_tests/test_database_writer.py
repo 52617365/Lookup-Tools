11bc7980b692b898c2086450320e28db4c388c70
52617365 <rasengithub@protonmail.com>
2022-11-17 13:07:35 +0200

    csv_writer renamed to database_writer.py
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

---
+++ DatabaseFile/database_file_tests/test_database_writer.py
@@ -0,0 +1,34 @@
+import json
+import os
+import unittest
+
+import pandas as pd
+
+from DatabaseFile.database_writer import DatabaseWriter
+
+
+def get_relative_path_to_file(relative_path_to_file: str) -> str:
+    dir_name = os.path.dirname(__file__)
+    relative_path = os.path.join(dir_name, relative_path_to_file)
+    return relative_path
+
+
+class TestCsvWriter(unittest.TestCase):
+    hashes_file = "file_hashes.txt"
+
+    def test_pandas_dataframe_json_conversion(self):
+        csv_data_to_write_as_json = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
+        converted_json_data = csv_data_to_write_as_json.to_json(orient='records')
+        json.loads(converted_json_data)
+
+    def test_unique_identifier_gets_generated_correctly(self):
+        csv_to_hash = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+
+        writer = DatabaseWriter("", csv_to_hash, self.hashes_file)
+
+        sha256_hash_to_expect = 'f90d860c5753d69b89d375e53ff8a9644f28c9ffe83cf1daa8de641d8d37ab07'
+        self.assertEqual(sha256_hash_to_expect, writer.file_identifier)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- DatabaseFile/database_writer.py
+++ DatabaseFile/database_writer.py
@@ -1,27 +1,27 @@
 import pandas as pd
 
-from hasher.hash import Hasher
-from hasher.hash_file_path import get_file_name_for_invalid_hashes_file
+from hashing.hash import get_sha256_hash_from
+from hashing.hash_file_path import get_file_name_for_invalid_hashes_file
 
 
 class DatabaseWriter:
     def __init__(self, writing_file_path: str, data_to_write: pd.DataFrame, hash_file_path: str):
         self.__path_to_writing_file = writing_file_path
         self.__data_to_write = data_to_write
-        self.__file_identifier = Hasher(self.__data_to_write).get_sha256_hash()
         self.__hash_file_path = hash_file_path
+        self.file_identifier = get_sha256_hash_from(data_to_write)
 
     def write_as_json(self):
         self.__data_to_write.to_json(self.__path_to_writing_file, orient='records')
 
     def write_valid_file_hash_to_logs(self):
         with open(self.__hash_file_path, "a") as valid_hashes:
-            valid_hashes.write(self.__file_identifier + "\n")
+            valid_hashes.write(self.file_identifier + "\n")
 
     def write_invalid_file_hash_to_logs(self):
         invalid_hashes_file_path = get_file_name_for_invalid_hashes_file(self.__hash_file_path)
         with open(invalid_hashes_file_path, "a") as invalid_hashes:
-            invalid_hashes.write(self.__file_identifier + "\n")
+            invalid_hashes.write(self.file_identifier + "\n")
 
     def file_is_unique(self) -> bool:
         return not self.__hash_is_already_in_hashes_file()
@@ -29,6 +29,6 @@ class DatabaseWriter:
     def __hash_is_already_in_hashes_file(self):
         with open(self.__hash_file_path, "r") as hashes:
             for line in hashes:
-                if line.strip() == self.__file_identifier:
+                if line.strip() == self.file_identifier:
                     return True
         return False
