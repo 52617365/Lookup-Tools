caa4cfaa29d0976bad79606263d2fea9b830d3f2
52617365 <rasengithub@protonmail.com>
2022-11-17 14:09:46 +0200

    JsonWriter.py now by default writes to a file called {file_name}.json.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseIO/JsonWriter.py
+++ DatabaseIO/JsonWriter.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 
 class JsonWriter:
-    def __init__(self, writing_file_path: str, data_to_write: pd.DataFrame):
-        self.__path_to_writing_file = writing_file_path
+    def __init__(self, database_file_name: str, data_to_write: pd.DataFrame):
+        self.database_file_name = database_file_name
         self.__data_to_write = data_to_write
 
     def write_as_json(self):
-        self.__data_to_write.to_json(self.__path_to_writing_file, orient='records')
+        self.__data_to_write.to_json(self.get_json_file_name(), orient='records')
+
+    def get_json_file_name(self):
+        return self.database_file_name + ".json"
--- main/user_arguments.py
+++ main/user_arguments.py
@@ -5,8 +5,8 @@ class CommandLineArguments:
 
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Parse delimited files into JSON strings')
-        self.parser.add_argument('-i', '--input', type=str, help='input file to parse', required=True)
-        self.parser.add_argument('-o', '--output', type=str, help='output file to write json to', required=True)
+        self.parser.add_argument('-i', '--input', type=str, help='The delimited file that should be parsed to JSON',
+                                 required=True)
         self.parser.add_argument('-g', '--glob', default=False, type=bool,
                                  help='A boolean value to determine if the input is a glob', required=False)
         self.args = self.parser.parse_args()
