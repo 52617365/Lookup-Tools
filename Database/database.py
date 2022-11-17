a51cfbf2da1c534533d4215f73cc4894c290400f
52617365 <rasengithub@protonmail.com>
2022-11-17 13:06:26 +0200

    get_file_name() added to database_reader.py
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Database/database.py
+++ Database/database.py
@@ -7,20 +7,13 @@ class Database:
     """Initializing this class can possibly fail if the database format in invalid."""
 
     def __init__(self, database_path: str, additional_database_information: DataFrame):
-        reader = DatabaseReader(database_path)
-        self.__database_contents = reader.get_database_as_dataframe()
-        self.__database_name = reader.get_database_name()
-
+        self.reader = DatabaseReader(database_path)
         self.__additional_information = additional_database_information
 
-    def combine(self) -> DataFrame:
-        database_contents_with_additional_information = self.__set_additional_information_to_database()
-        return database_contents_with_additional_information
-
-    def __set_additional_information_to_database(self):
+    def set_additional_information_to_database(self):
         breach_date = self.__get_breach_date_from_additional_database_information()
 
-        database_contents_with_additional_information = self.__database_contents
+        database_contents_with_additional_information = self.reader.get_database_as_dataframe()
 
         self.__set_db_name(database_contents_with_additional_information)
         self.__set_breach_date(breach_date, database_contents_with_additional_information)
@@ -30,13 +23,13 @@ class Database:
     def __get_breach_date_from_additional_database_information(self) -> str | None:
         try:
             breach_date = self.__additional_information.loc[
-                self.__additional_information['database'] == self.__database_name, 'dumped'].item()
+                self.__additional_information['database'] == self.reader.get_file_name(), 'dumped'].item()
             return breach_date
         except ValueError:
             return None
 
     def __set_db_name(self, database_contents_with_additional_information):
-        database_contents_with_additional_information["database_name"] = self.__database_name
+        database_contents_with_additional_information["database_name"] = self.reader.get_file_name()
 
     @staticmethod
     def __set_breach_date(breach_date: str, database_contents_with_additional_information: DataFrame):
--- DatabaseFile/database_reader.py
+++ DatabaseFile/database_reader.py
@@ -1,31 +1,28 @@
+import os
 import warnings
 
 import pandas as pd
 from pandas import DataFrame
 from pandas.errors import ParserWarning
 
-from DatabaseFile.database_file import DatabaseFile
-
 
 class DatabaseReader:
     def __init__(self, database_file_path: str):
-        self.__database_file = DatabaseFile(database_file_path)
+        self.database_path = database_file_path
 
     def get_database_as_dataframe(self) -> pd.DataFrame:
         # try:
         csv_file: DataFrame = self.__get_csv_with_custom_delimiter_turning_warnings_into_errors()
         return csv_file
 
-    # except Exception as invalid_database_format:
-    #     # TODO: we have a write_invalid_file_hash_to_logs() function to use when this exception is raised.
-    #     # Figure out if we somehow want to catch it here or in the caller of this function.
-    #     raise invalid_database_format
-
     def __get_csv_with_custom_delimiter_turning_warnings_into_errors(self) -> DataFrame:
         # Hack to turn warnings into errors.
         with warnings.catch_warnings():
             warnings.simplefilter("error", category=ParserWarning)
-            csv_file = pd.read_csv(self.__database_file.get_file_path(), engine='python', sep=None,
+            csv_file = pd.read_csv(self.database_path, engine='python', sep=None,
                                    skipinitialspace=True,
                                    index_col=False)
             return csv_file
+
+    def get_file_name(self) -> str:
+        return os.path.splitext(os.path.basename(self.database_path))[0]
