933bd667ee954da9ab56fe715776ae8f1fe3c4a2
52617365 <rasengithub@protonmail.com>
2022-11-17 10:57:58 +0200

    database_file.py no longer validates that the file exists.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseFile/database_file.py
+++ DatabaseFile/database_file.py
@@ -3,24 +3,11 @@ import os
 
 class DatabaseFile:
     def __init__(self, file_path):
-        self.__raise_io_error_if_file_does_not_exist(file_path)
-        self.__file_path = file_path
         self.__file_name = self.__get_file_without_path_or_extension(file_path)
-
-    def __raise_io_error_if_file_does_not_exist(self, file_path: str):
-        if not self.__is_valid_file(file_path):
-            raise IOError(F"File does not exist: {file_path}")
 
     @staticmethod
     def __get_file_without_path_or_extension(file_name: str) -> str:
         return os.path.splitext(os.path.basename(file_name))[0]
 
-    @staticmethod
-    def __is_valid_file(file_path: str) -> bool:
-        return os.path.isfile(file_path)
-
     def get_file_name(self) -> str:
         return self.__file_name
-
-    def get_file_path(self) -> str:
-        return self.__file_path
