692c8b67bd37d868c0c7713e213ce2d5ce273f61
52617365 <rasengithub@protonmail.com>
2022-11-17 14:38:27 +0200

    HashWriter.py now takes in file handles as ctor parameters instead of opening them over and over again.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseIO/HashWriter.py
+++ DatabaseIO/HashWriter.py
@@ -3,35 +3,32 @@ import hashlib
 import pandas as pd
 from pandas import DataFrame
 
-from hashing.HashFilePath import get_file_name_for_invalid_hashes_file
-
 
 class HashWriter:
-    def __init__(self, data_to_write: pd.DataFrame, hash_file_path: str):
-        self.__hash_file_path = hash_file_path
-        self.file_identifier = self.get_sha256_hash_from(data_to_write)
+    def __init__(self, data_to_write: pd.DataFrame, valid_hashes_file, invalid_hashes_file):
+        self.valid_hashes_file = valid_hashes_file
+        self.invalid_hashes_file = invalid_hashes_file
+        self.file_identifier = self.__get_sha256_hash_from(data_to_write)
 
     @staticmethod
-    def get_sha256_hash_from(data_to_write: DataFrame) -> str:
+    def __get_sha256_hash_from(data_to_write: DataFrame) -> str:
         file_data = data_to_write.to_string()
         return hashlib.sha256(file_data.encode('utf-8')).hexdigest()
 
     def write_valid_file_hash_to_logs(self):
-        with open(self.__hash_file_path, "a") as valid_hashes:
-            valid_hashes.write(self.file_identifier + "\n")
+        self.valid_hashes_file.write(self.file_identifier + "\n")
 
     def write_invalid_file_hash_to_logs(self):
-        invalid_hashes_file_path = get_file_name_for_invalid_hashes_file(self.__hash_file_path)
-        with open(invalid_hashes_file_path, "a") as invalid_hashes:
-            invalid_hashes.write(self.file_identifier + "\n")
+        self.invalid_hashes_file.write(self.file_identifier + "\n")
 
     def file_is_unique(self) -> bool:
         return not self.__hash_is_already_in_hashes_file()
 
-    # TODO: could load the hashes file once into a list then append to it instead of looking at hashes file each time.
     def __hash_is_already_in_hashes_file(self):
-        with open(self.__hash_file_path, "r") as hashes:
-            for line in hashes:
-                if line.strip() == self.file_identifier:
-                    return True
+        valid_hashes = self.valid_hashes_file.readlines()
+        invalid_hashes = self.invalid_hashes_file.readlines()
+        hashes = valid_hashes + invalid_hashes
+        for line in hashes:
+            if line.strip() == self.file_identifier:
+                return True
         return False
