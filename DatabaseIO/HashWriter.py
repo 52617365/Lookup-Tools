24425d66328d124bf84cbb9d38aafaf6d2ba8db8
52617365 <rasengithub@protonmail.com>
2022-11-17 13:26:06 +0200

    Database writer split into hash writer and json writer.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseIO/HashWriter.py
+++ DatabaseIO/HashWriter.py
@@ -1,18 +1,20 @@
+import hashlib
+
 import pandas as pd
+from pandas import DataFrame
 
-from hashing.hash import get_sha256_hash_from
-from hashing.hash_file_path import get_file_name_for_invalid_hashes_file
+from hashing.HashFilePath import get_file_name_for_invalid_hashes_file
 
 
-class DatabaseWriter:
-    def __init__(self, writing_file_path: str, data_to_write: pd.DataFrame, hash_file_path: str):
-        self.__path_to_writing_file = writing_file_path
-        self.__data_to_write = data_to_write
+class HashWriter:
+    def __init__(self, data_to_write: pd.DataFrame, hash_file_path: str):
         self.__hash_file_path = hash_file_path
-        self.file_identifier = get_sha256_hash_from(data_to_write)
+        self.file_identifier = self.get_sha256_hash_from(data_to_write)
 
-    def write_as_json(self):
-        self.__data_to_write.to_json(self.__path_to_writing_file, orient='records')
+    @staticmethod
+    def get_sha256_hash_from(data_to_write: DataFrame) -> str:
+        file_data = data_to_write.to_string()
+        return hashlib.sha256(file_data.encode('utf-8')).hexdigest()
 
     def write_valid_file_hash_to_logs(self):
         with open(self.__hash_file_path, "a") as valid_hashes:
@@ -26,6 +28,7 @@ class DatabaseWriter:
     def file_is_unique(self) -> bool:
         return not self.__hash_is_already_in_hashes_file()
 
+    # TODO: could load the hashes file once into a list then append to it instead of looking at hashes file each time.
     def __hash_is_already_in_hashes_file(self):
         with open(self.__hash_file_path, "r") as hashes:
             for line in hashes:
