4481a01bedd55a92abf0a7c26433ceca9f867011
52617365 <rasengithub@protonmail.com>
2022-11-18 10:23:50 +0200

    Combiner now takes in the database file contents instead of reading it.
    - This makes testing easier and supports separation of concerns.
    
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Database/DatabaseCombiner.py
+++ Database/DatabaseCombiner.py
@@ -1,33 +1,25 @@
 import os
 
-import pandas as pd
 from pandas import DataFrame
-
-from DatabaseIO.DatabaseReader import DatabaseReader
 
 
 class DatabaseCombiner:
     def __init__(self, additional_database_information: DataFrame):
         self.additional_database_information = additional_database_information
 
-    def combine(self, database_file_path: str) -> pd.DataFrame:
-        reader = DatabaseReader(database_file_path, self.additional_database_information)
+    def combine(self, database_contents: DataFrame, database_file_path: str) -> DataFrame:
+        self.__set_db_name(database_contents, database_file_path)
+        self.__set_breach_date(database_contents, database_file_path)
+        return database_contents
 
-        database = reader.get_database_as_dataframe()
-
-        self.__set_db_name(database, database_file_path)
-        self.__set_breach_date(database, database_file_path)
-
-        return database
-
-    def __set_db_name(self, database: DataFrame, database_file_path: str):
+    def __set_db_name(self, database_contents: DataFrame, database_file_path: str):
         database_name = self.get_file_name(database_file_path)
-        database["database_name"] = database_name
+        database_contents["database_name"] = database_name
 
-    def __set_breach_date(self, database: DataFrame, database_file_path: str):
+    def __set_breach_date(self, database_contents: DataFrame, database_file_path: str):
         breach_date = self.get_breach_date_from_additional_database_information(database_file_path)
         if breach_date is not None:
-            database["breach_date"] = breach_date
+            database_contents["breach_date"] = breach_date
 
     def get_breach_date_from_additional_database_information(self, database_file_path: str) -> str | None:
         try:
