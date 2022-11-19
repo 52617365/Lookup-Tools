7c0186793d67a28b8d15db343b24810e6678bb18
52617365 <rasengithub@protonmail.com>
2022-11-19 16:33:49 +0200

    Added env file support to HashWriter.py to have all required information for connection.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseIO/HashWriter.py
+++ DatabaseIO/HashWriter.py
@@ -1,9 +1,32 @@
 import hashlib
+import pathlib
 
+import pymongo
+from dotenv import dotenv_values
 from pandas import DataFrame
 
-from hashing.HashFilePath import get_file_name_for_invalid_hashes_file
 
+class HashWriterConnection:
+    def __init__(self):
+        env = self.get_mongodb_env_variables()
+        client = pymongo.MongoClient(env["CONNECTION_STRING"])
+        database = client[env["DATABASE_NAME"]]
+        self.collection = database[env["COLLECTION_NAME"]]
+
+    @staticmethod
+    def get_mongodb_env_variables():
+        current_path = pathlib.Path(__file__).parent.resolve()
+        config = dotenv_values(F"{current_path}\.env")
+        HashWriterConnection.__terminate_if_env_values_invalid(config)
+        return config
+
+    @staticmethod
+    def __terminate_if_env_values_invalid(config: dict):
+        required_keys = ['DATABASE_NAME', 'COLLECTION_NAME', 'CONNECTION_STRING']
+
+        for key in required_keys:
+            if config[key] is None:
+                quit("Missing key in .env file: " + key)
 
 # TODO: change this to use a database.
 class HashWriter:
