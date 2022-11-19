05bf171cff030d976c4b2e330fc43cf16484c2c1
52617365 <rasengithub@protonmail.com>
2022-11-19 10:00:47 +0200

    DatabaseReader.py now returns the file identifier on top of the csv.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseIO/DatabaseReader.py
+++ DatabaseIO/DatabaseReader.py
@@ -4,10 +4,18 @@ import pandas as pd
 from pandas import DataFrame
 from pandas.errors import ParserWarning
 
+from DatabaseIO.HashWriter import HashWriter
+
 
 class DatabaseReader:
-    def __init__(self, database_file_path: str):
+    def __init__(self, database_file_path: str, hasher: HashWriter):
         self.database_file_path = database_file_path
+        self.hasher = hasher
+
+    def get_database(self) -> (pd.DataFrame, str):
+        csv_file: DataFrame = self.get_database_as_dataframe()
+        file_identifier = self.hasher.get_sha256_hash_from(csv_file)
+        return csv_file, file_identifier
 
     def get_database_as_dataframe(self) -> pd.DataFrame:
         csv_file: DataFrame = self.__get_csv_with_custom_delimiter_turning_warnings_into_errors()
