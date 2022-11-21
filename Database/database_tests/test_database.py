a02d77432e345a71c42b9ed47cd17985b2c13053
52617365 <rasengithub@protonmail.com>
2022-11-21 16:48:42 +0200

    test_database.py no longer uses file system, instead it uses a fake file system.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Database/database_tests/test_database.py
+++ Database/database_tests/test_database.py
@@ -1,20 +1,14 @@
-import os
 import unittest
 
 import pandas as pd
+from pyfakefs.fake_filesystem_unittest import TestCase
 
 from Database.DatabaseCombiner import DatabaseCombiner
 
 
-def get_relative_path_to_file(relative_path_to_file: str) -> str:
-    dir_name = os.path.dirname(__file__)
-    relative_path = os.path.join(dir_name, relative_path_to_file)
-    return relative_path
-
-
-class TestDatabase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.testing_file_path = os.path.join('Database', 'database_tests', 'files', 'testing_file.txt')
+class TestDatabase(TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
 
     def test_get_breach_date_from_additional_database_information(self):
         additional_information_about_databases = pd.DataFrame(
@@ -36,15 +30,20 @@ class TestDatabase(unittest.TestCase):
                 additional_information_about_databases['database'] == 'this_database_does_not_exist', 'dumped'].item()
 
     def test_set_additional_information_to_database(self):
+        testing_file_path = self.create_fake_file("testing_file.txt", "test;test2;test3\nasd1,asd2,asd3")
         additional_information_about_databases = pd.DataFrame({'database': ["testing_file"], 'entries': [15271696],
                                                                'dumped': ["2011-05-21"]})
 
         our_loaded_database = DatabaseCombiner(additional_information_about_databases)
 
         database_contents = pd.DataFrame({'test': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
-        combined_database = our_loaded_database.combine(database_contents, self.testing_file_path)
+        combined_database = our_loaded_database.combine(database_contents, testing_file_path)
         self.assertEqual("testing_file", combined_database["database_name"].item())
         self.assertEqual("2011-05-21", combined_database["breach_date"].item())
+
+    def create_fake_file(self, testing_file_path: str, contents: str):
+        self.fs.create_file(testing_file_path, contents=contents)
+        return testing_file_path
 
 
 if __name__ == '__main__':
