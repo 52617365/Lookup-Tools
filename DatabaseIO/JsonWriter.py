4c93fedeca9a605563a1eb5f4a5498c36f7bb359
52617365 <rasengithub@protonmail.com>
2022-11-22 09:07:34 +0200

    DatabaseWriter.py now has a test that tests the write_as_json function.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseIO/JsonWriter.py
+++ DatabaseIO/JsonWriter.py
@@ -4,15 +4,15 @@ from Database.DatabaseCombiner import DatabaseCombiner
 
 
 class JsonWriter:
-    def __init__(self, database_file, data_to_write: pd.DataFrame):
+    def __init__(self, database_file: str, data_to_write: pd.DataFrame):
         self.database_file_name = DatabaseCombiner.get_file_name(database_file)
         self.__data_to_write = data_to_write
 
     def write_as_json(self):
         try:
-            self.__data_to_write.to_json(F"parsed/{self.get_json_file_name()}", orient='records')
+            self.__data_to_write.to_json(F"parsed/{self.__get_json_file_name()}", orient='records')
         except OSError:
             quit("Make sure the 'parsed' folder exists")
 
-    def get_json_file_name(self):
+    def __get_json_file_name(self):
         return self.database_file_name + ".json"
--- DatabaseIO/database_file_tests/test_json_writer.py
+++ DatabaseIO/database_file_tests/test_json_writer.py
@@ -2,11 +2,30 @@ import json
 import unittest
 
 import pandas as pd
+from pyfakefs.fake_filesystem_unittest import TestCase
 
 from DatabaseIO.HashWriter import HashWriter
+from DatabaseIO.JsonWriter import JsonWriter
 
 
-class TestDatabaseWriter(unittest.TestCase):
+class TestDatabaseWriter(TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_write_as_json(self):
+        fake_file_name = "parsed/test.json"
+        self.fs.create_file(fake_file_name)
+
+        csv_data_to_write_as_json = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
+        writer = JsonWriter(fake_file_name, csv_data_to_write_as_json)
+        writer.write_as_json()
+
+        expected_json_data = "[{\"dir\":\"asd1\",\"test2\":\"asd2\",\"test3\":\"asd3\"}]"
+
+        with open(fake_file_name, 'r') as f:
+            json_data = f.read()
+            assert json_data == expected_json_data
+
     def test_pandas_dataframe_json_conversion(self):
         csv_data_to_write_as_json = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
         converted_json_data = csv_data_to_write_as_json.to_json(orient='records')
