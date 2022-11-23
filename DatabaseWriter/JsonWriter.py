2d6360123060c4095d02cda78f4cc733ba3ace65
52617365 <rasengithub@protonmail.com>
2022-11-23 10:02:30 +0200

    Added function to JsonWriter.py to add additional information about a database to databases collection. Tests pass.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- DatabaseWriter/JsonWriter.py
+++ DatabaseWriter/JsonWriter.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Mapping, Any
 
 import pandas as pd
@@ -5,13 +6,30 @@ from pymongo.collection import Collection
 
 
 class JsonWriter:
-    def __init__(self, data_to_write: pd.DataFrame, mongo_collection: Collection[Mapping[str, Any]]):
+    def __init__(self, data_to_write: pd.DataFrame, mongo_data_collection: Collection[Mapping[str, Any]] | None,
+                 mongo_databases_collection: Collection[Mapping[str, Any]] | None):
         self.__data_to_write = data_to_write
-        self.mongo_collection = mongo_collection
+        self.mongo_data_collection = mongo_data_collection
+        self.mongo_databases_collection = mongo_databases_collection
 
     def write_as_json(self):
         try:
             data_to_write_in_json = self.__data_to_write.to_dict(orient='records')
-            self.mongo_collection.insert_many(data_to_write_in_json)
+            self.mongo_data_collection.insert_many(data_to_write_in_json)
+        except Exception as e:
+            quit("There was an error while writing to MongoDB: " + str(e))
+
+    def write_additional_information(self):
+        try:
+            database_name = self.__data_to_write['database_name'].iloc[0]
+            lines_in_database = len(self.__data_to_write)
+            breach_date = self.__data_to_write['breach_date'].iloc[0]
+            if breach_date is None:
+                self.mongo_databases_collection.insert_one(
+                    {'database_name': database_name, 'lines_in_database': lines_in_database, 'added': datetime.now()})
+            else:
+                self.mongo_databases_collection.insert_one(
+                    {'database_name': database_name, 'lines_in_database': lines_in_database, 'breach_date': breach_date,
+                     'added': datetime.now()})
         except Exception as e:
             quit("There was an error while writing to MongoDB: " + str(e))
--- DatabaseWriter/database_file_tests/test_json_writer.py
+++ DatabaseWriter/database_file_tests/test_json_writer.py
@@ -7,19 +7,30 @@ from pyfakefs.fake_filesystem_unittest import TestCase
 from DatabaseWriter.JsonWriter import JsonWriter
 
 
-class TestDatabaseWriter(TestCase):
+class TestJsonWriter(TestCase):
     def setUp(self):
         self.setUpPyfakefs()
 
     def test_write_as_json(self):
         client = mongomock.MongoClient().test.test_collection
-        collection = client.db.collection
+        data_collection = client.db.collection
 
         csv_data = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
 
-        writer = JsonWriter(csv_data, collection)
+        writer = JsonWriter(csv_data, data_collection, None)
         writer.write_as_json()
-        self.assertTrue(collection.find_one({'dir': "asd1"}, {'_id': 1}))
+        self.assertTrue(data_collection.find_one({'dir': "asd1"}, {'_id': 1}))
+
+    def test_write_additional_information(self):
+        client = mongomock.MongoClient().test.test_collection
+        databases_collection = client.db.collection
+
+        csv_data = pd.DataFrame({"database_name": ["test"], "breach_date": ["2020-01-01"]})
+
+        writer = JsonWriter(csv_data, None, databases_collection)
+        writer.write_additional_information()
+        self.assertTrue(databases_collection.find_one({'database_name': "test"}, {'_id': 1}))
+        self.assertTrue(databases_collection.find_one({'breach_date': "2020-01-01"}, {'_id': 1}))
 
 
 if __name__ == '__main__':
