7a825e914bf95c5cbf11c9bd0062402369a81c60
52617365 <rasengithub@protonmail.com>
2022-11-27 08:53:09 +0200

    added new argument for json.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Usage/UserArguments.py
+++ Usage/UserArguments.py
@@ -11,6 +11,9 @@ class CommandLineArguments:
                                  help='The path to the additional database information', required=True)
         self.parser.add_argument('-g', '--glob', default=False, type=bool,
                                  help='A boolean value to determine if the input is a glob', required=False)
+        self.parser.add_argument('-j', '--json', default=False, type=bool,
+                                 help='A boolean flag to determine if the file user is trying to import is JSON:',
+                                 required=False)
         self.args = self.parser.parse_args()
 
     def get(self):
