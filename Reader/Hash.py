784169c24e5573ea7ffd3ff8b3345b6ae6e2603f
52617365 <rasengithub@protonmail.com>
2022-11-27 09:38:25 +0200

    Moved generating hash to the Hash class. This simplified stuff by alot.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

---
+++ Reader/Hash.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+class Hash:
+    def __init__(self, path_to_file: str):
+        self.path_to_file = path_to_file
+
+    def get_hash_from_file_contents(self) -> str:
+        try:
+            with open(self.path_to_file, "r") as file:
+                file_contents = file.read()
+                return self.__get_blake2b_hash_from(file_contents)
+        except OSError:
+            quit("Could not open file: " + self.path_to_file)
+
+    @staticmethod
+    def __get_blake2b_hash_from(file_data: str) -> str:
+        file_hash = hashlib.blake2b(file_data.encode('utf-8')).hexdigest()
+        return file_hash
