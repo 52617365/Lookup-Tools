6820c73d78473e09141e1cae903c6b074e816258
52617365 <rasengithub@protonmail.com>
2022-11-27 09:19:18 +0200

    added test for getting database as json.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Reader/database_reader_tests/test_database_reader.py
+++ Reader/database_reader_tests/test_database_reader.py
@@ -19,7 +19,7 @@ class TestDatabaseReader(TestCase):
         testing_file_path = self.create_fake_file("testing_file.csv", "dir,test2,test3\nasd1,asd2,asd3")
 
         example_delimited_file = DatabaseReader(testing_file_path, None)
-        data = example_delimited_file.get_database_as_dataframe()
+        data = example_delimited_file.get_database_from_csv()
         expected_data = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
         self.assertEqual(data.equals(expected_data), True)
 
@@ -27,7 +27,7 @@ class TestDatabaseReader(TestCase):
         testing_file_path = self.create_fake_file("testing_file.csv", "dir:test2:test3\nasd1:asd2:asd3")
 
         example_delimited_file = DatabaseReader(testing_file_path, None)
-        data = example_delimited_file.get_database_as_dataframe()
+        data = example_delimited_file.get_database_from_csv()
         expected_data = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
         self.assertEqual(data.equals(expected_data), True)
 
@@ -35,7 +35,7 @@ class TestDatabaseReader(TestCase):
         testing_file_path = self.create_fake_file("testing_file.csv", "dir|test2|test3\nasd1|asd2|asd3")
 
         example_delimited_file = DatabaseReader(testing_file_path, None)
-        data = example_delimited_file.get_database_as_dataframe()
+        data = example_delimited_file.get_database_from_csv()
         expected_data = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
         self.assertEqual(data.equals(expected_data), True)
 
@@ -43,7 +43,7 @@ class TestDatabaseReader(TestCase):
         testing_file_path = self.create_fake_file("testing_file.csv", "dir.test2.test3\nasd1.asd2.asd3")
 
         example_delimited_file = DatabaseReader(testing_file_path, None)
-        data = example_delimited_file.get_database_as_dataframe()
+        data = example_delimited_file.get_database_from_csv()
         expected_data = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
         self.assertEqual(data.equals(expected_data), True)
 
@@ -51,7 +51,7 @@ class TestDatabaseReader(TestCase):
         testing_file_path = self.create_fake_file("testing_file.csv", "dir\ttest2\ttest3\nasd1\tasd2\tasd3")
 
         example_delimited_file = DatabaseReader(testing_file_path, None)
-        data = example_delimited_file.get_database_as_dataframe()
+        data = example_delimited_file.get_database_from_csv()
         expected_data = pd.DataFrame({'dir': ["asd1"], 'test2': ["asd2"], 'test3': ["asd3"]})
         self.assertEqual(data.equals(expected_data), True)
 
@@ -59,31 +59,49 @@ class TestDatabaseReader(TestCase):
         with self.assertRaises(Exception):
             testing_file_path = self.create_fake_file("invalid_format_file.csv", "field1,field2\nvalue1,value2,value3")
             f = DatabaseReader(testing_file_path, None)
-            f.get_database_as_dataframe()
+            f.get_database_from_csv()
 
     @patch('Connection.DatabaseConnection.pymongo.MongoClient.server_info')
     @patch('Connection.DatabaseConnection.dotenv_values')
     def test_get_database(self, mock_dotenv_values, mongo_server_info):
         testing_file_path = self.create_fake_file("testing_file.csv", "field1,field2,field3\nasd1,asd2,asd3")
 
-        self.avoid_exit_if_instance_mongo_instance_does_not_exist(mongo_server_info)
+        hash_writer = self.init_get_database(mock_dotenv_values, mongo_server_info)
 
+        reader = DatabaseReader(testing_file_path, hash_writer)
+        data_frame, file_identifier = reader.get_database()
+
+        expected_data_frame = pd.DataFrame({'field1': ["asd1"], 'field2': ["asd2"], 'field3': ["asd3"]})
+        expected_file_identifier = "0a85dea3cfe57ca01ab859e954acde77ae8caf899647e4d1f9c01ed55995a03fc7445ada4ac67567390082532d5996876bed744677a502f415bfce26ee3847a0"
+        self.assertEqual(data_frame.equals(expected_data_frame), True)
+        self.assertEqual(expected_file_identifier, file_identifier)
+
+    @patch('Connection.DatabaseConnection.pymongo.MongoClient.server_info')
+    @patch('Connection.DatabaseConnection.dotenv_values')
+    def test_get_database_as_json(self, mock_dotenv_values, mongo_server_info):
+        testing_file_path = self.create_fake_file("testing_file.json",
+                                                  '{"field1": "asd1", "field2": "asd2", "field3": "asd3"}')
+
+        hash_writer = self.init_get_database(mock_dotenv_values, mongo_server_info)
+
+        reader = DatabaseReader(testing_file_path, hash_writer, True)
+        data_frame, file_identifier = reader.get_database()
+
+        expected_data_frame = pd.DataFrame({'field1': ["asd1"], 'field2': ["asd2"], 'field3': ["asd3"]})
+        expected_file_identifier = "d289a3d105ee93820097961254f121a551729a3095963172b7130987f36f941390cc08a4b4c18e2d282fd578e2b54d2fddc1a937ebbaef8a42708b0c842a71b2"
+        self.assertEqual(data_frame.equals(expected_data_frame), True)
+        self.assertEqual(expected_file_identifier, file_identifier)
+
+    def init_get_database(self, mock_dotenv_values, mongo_server_info):
+        self.avoid_exit_if_instance_mongo_instance_does_not_exist(mongo_server_info)
         mock_dotenv_values.return_value = OrderedDict(
             {"CONNECTION_STRING": "test_connection_string", "DATABASE_NAME": "test_database_name",
              "HASHES_COLLECTION_NAME": "test_collection_name",
              "DATABASES_COLLECTION_NAME": "test_databases_collection_name",
              "DATA_COLLECTION_NAME": "test_data_collection_name"})
-
         hash_collection = DatabaseConnection().hash_collection
         hash_writer = HashWriter(hash_collection)
-
-        reader = DatabaseReader(testing_file_path, hash_writer)
-        csv_file, file_identifier = reader.get_database()
-
-        expected_csv_file = pd.DataFrame({'field1': ["asd1"], 'field2': ["asd2"], 'field3': ["asd3"]})
-        expected_file_identifier = "0a85dea3cfe57ca01ab859e954acde77ae8caf899647e4d1f9c01ed55995a03fc7445ada4ac67567390082532d5996876bed744677a502f415bfce26ee3847a0"
-        self.assertEqual(csv_file.equals(expected_csv_file), True)
-        self.assertEqual(expected_file_identifier, file_identifier)
+        return hash_writer
 
     @staticmethod
     def avoid_exit_if_instance_mongo_instance_does_not_exist(mongo_server_info):
