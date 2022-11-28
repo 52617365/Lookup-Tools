24383ee04aa7d19cac561809bef66803399a5ece
52617365 <rasengithub@protonmail.com>
2022-11-28 22:06:16 +0200

    DatabaseReader.py now takes optional ignored fields into consideration when reading csv.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Reader/DatabaseReader.py
+++ Reader/DatabaseReader.py
@@ -17,7 +17,8 @@ class DatabaseReader:
         self.is_json = is_json
         self.hash = Hash(self.database_file_path)
 
-    def terminate_on_invalid_arguments(self, file_format, is_json):
+    @staticmethod
+    def terminate_on_invalid_arguments(file_format, is_json):
         if file_format is None and is_json is False:
             quit("file_format can only be None if is_json is True")
 
@@ -30,19 +31,38 @@ class DatabaseReader:
         try:
             if self.is_json:
                 data_frame = self.get_database_from_json()
-                return data_frame
             else:
-                data_frame = self.get_database_from_csv()
-                return data_frame
+                data_frame = self.get_database_from_csv_with_optional_ignored_fields()
+            return data_frame
         except ParserWarning:
             quit(F"Format of database in path {self.database_file_path} is not correct")
 
-    def get_database_from_csv(self) -> DataFrame:
+    def get_database_from_csv_with_optional_ignored_fields(self) -> DataFrame:
         with warnings.catch_warnings():
             warnings.simplefilter("error", category=ParserWarning)
-            csv_file = pd.read_csv(self.database_file_path, sep=self.file_format.delimiter,
-                                   names=self.file_format.fields, header=None, index_col=False)
+            if self.ignored_fields_exist():
+                csv_file = self.get_csv_without_without_ignored_fields()
+            else:
+                csv_file = self.get_csv_with_all_fields()
             return csv_file
+
+    def get_csv_with_all_fields(self):
+        csv_file = pd.read_csv(self.database_file_path, sep=self.file_format.file_delimiter,
+                               names=self.file_format.fields, header=None, index_col=False)
+        return csv_file
+
+    def get_csv_without_without_ignored_fields(self):
+        fields_to_keep = self.get_fields_we_want_to_keep()
+        csv_file = pd.read_csv(self.database_file_path, sep=self.file_format.file_delimiter,
+                               names=self.file_format.fields, header=None, index_col=False,
+                               usecols=fields_to_keep)
+        return csv_file
+
+    def ignored_fields_exist(self):
+        return len(self.file_format.ignored_fields) is not 0
+
+    def get_fields_we_want_to_keep(self):
+        return list(filter(lambda x: x not in self.file_format.ignored_fields, self.file_format.fields))
 
     def get_database_from_json(self):
         try:
