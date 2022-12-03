8683b9d4de8d83265ace9eb9b339b7ec0c0fb613
52617365 <rasengithub@protonmail.com>
2022-12-03 11:58:54 +0200

    Hash now gets generated from chunks.
    This is done to avoid making ram an issue.
    
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Reader/Hash.py
+++ Reader/Hash.py
@@ -8,12 +8,19 @@ class Hash:
     def get_hash_from_file_contents(self) -> str:
         try:
             with open(self.path_to_file, "rb") as file:
-                file_contents = file.read()
-                return self.__get_blake2b_hash_from(file_contents)
+                blake2b_hash = self.get_hash_from_file_chunks(file)
+                return blake2b_hash
         except OSError:
             quit("Could not open file: " + self.path_to_file)
 
     @staticmethod
-    def __get_blake2b_hash_from(file_data: bytes) -> str:
-        file_hash = hashlib.blake2b(file_data).hexdigest()
-        return file_hash
+    def get_hash_from_file_chunks(file) -> str:
+        blake2b_hash = hashlib.blake2b()
+        for chunk in iter(lambda: file.read(128 * blake2b_hash.block_size), b""):
+            blake2b_hash.update(chunk)
+        return blake2b_hash.hexdigest()
+
+    # @staticmethod
+    # def __get_blake2b_hash_from(file_data: bytes) -> str:
+    #     file_hash = hashlib.blake2b(file_data).hexdigest()
+    #     return file_hash
