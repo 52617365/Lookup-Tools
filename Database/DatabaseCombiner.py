9459137443f05791c67fc789f968533a0a968ca0
52617365 <rasengithub@protonmail.com>
2022-12-05 15:33:28 +0200

    Made get_breach_date_from_additional_database_information and get_file_name_from_path normal functions because I use them all over the place.
    Signed-off-by: 52617365 <rasengithub@protonmail.com>

--- Database/DatabaseCombiner.py
+++ Database/DatabaseCombiner.py
@@ -12,24 +12,28 @@ class DatabaseCombiner:
         self.__set_breach_date(database_contents, database_file_path)
         return database_contents
 
-    def __set_db_name(self, database_contents: DataFrame, database_file_path: str):
-        database_name = self.get_file_name(database_file_path)
+    @staticmethod
+    def __set_db_name(database_contents: DataFrame, database_file_path: str):
+        database_name = get_file_name_from_path(database_file_path)
         database_contents["database_name"] = database_name
 
     def __set_breach_date(self, database_contents: DataFrame, database_file_path: str):
-        breach_date = self.get_breach_date_from_additional_database_information(database_file_path)
+        breach_date = get_breach_date_from_additional_database_information(database_file_path,
+                                                                           self.additional_database_information)
         if breach_date is not None:
             database_contents["breach_date"] = breach_date
 
-    def get_breach_date_from_additional_database_information(self, database_file_path: str) -> str | None:
-        try:
-            breach_date = self.additional_database_information.loc[
-                self.additional_database_information['database'] == self.get_file_name(
-                    database_file_path), 'dumped'].item()
-            return breach_date
-        except ValueError:
-            return None
 
-    @staticmethod
-    def get_file_name(database_file_path: str) -> str:
-        return os.path.splitext(os.path.basename(database_file_path))[0]
+def get_breach_date_from_additional_database_information(
+        database_file_path: str, additional_database_information) -> str | None:
+    try:
+        breach_date = additional_database_information.loc[
+            additional_database_information['database'] == get_file_name_from_path(
+                database_file_path), 'dumped'].item()
+        return breach_date
+    except ValueError:
+        return None
+
+
+def get_file_name_from_path(database_file_path: str) -> str:
+    return os.path.splitext(os.path.basename(database_file_path))[0]
